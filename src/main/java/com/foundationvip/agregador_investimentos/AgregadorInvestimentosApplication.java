c01ed84e313e74339174a0f849215a5253256cfd
Carlos Aurélio Souza de Assis <carlosync@gmail.com>
2024-06-23 22:46:13 -0400

    Criação do projeto e config do banco de dados

---
+++ src/main/java/com/foundationvip/agregador_investimentos/AgregadorInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.foundationvip.agregador_investimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestimentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgregadorInvestimentosApplication.class, args);
+    }
+
+}
